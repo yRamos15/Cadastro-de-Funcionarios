22a978c0e74115502f829fa89e161a7cb4a9d1c3
Davinvs22 <113136032+Davinvs22@users.noreply.github.com>
2022-09-20 14:57:40 -0400

    Atualização

---
+++ src/main/java/com/example/CadastrodeFuncionarios/CadastroDeFuncionariosApplication.java
@@ -0,0 +1,13 @@
+package com.example.CadastrodeFuncionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeFuncionariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeFuncionariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CadastrodeFuncionarios/CadastroDeFuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CadastrodeFuncionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeFuncionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
